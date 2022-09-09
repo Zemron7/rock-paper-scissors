b3cc0fb3a2395c1d336507997d3ec3db6a276dc9
Zemron7 <105683349+Zemron7@users.noreply.github.com>
2022-09-09 14:28:36 -0500

    Update server.js

--- server.js
+++ server.js
@@ -204,4 +204,4 @@ io.on("connection", socket =>{
     });
 })
 
-server.listen(5000, () => console.log("Server started on port 5000..."));
+server.listen(process.env.PORT || 5000, () => console.log("Server started on port 5000..."));
